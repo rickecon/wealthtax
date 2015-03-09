6527c0298d0e0acc5dfc242d5098de31f318076b
Evan Magnusson <evanmag42@gmail.com>
2015-03-09 14:59:00 -0600

    today

--- Python/income_nopoly.py
+++ Python/income_nopoly.py
@@ -20,31 +20,32 @@ This py-file creates the following other file(s):
 '''
 
 import numpy as np
+import pandas as pd
 import matplotlib
-# matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy.polynomial.polynomial as poly
 import scipy.optimize as opt
+from scipy import interpolate
 
 
 '''
 ------------------------------------------------------------------------
-    Generate Polynomials
+    Read Data for Ability Types
 ------------------------------------------------------------------------
-
+The data comes from the IRS.  We can either use wage or earnings data,
+in this version we are using earnings data.  The data is for individuals
+in centile groups for each age from 20 to 70.
 ------------------------------------------------------------------------
 '''
 
-# Vals for: .25 .25 .2 .1 .1 .09 .01
-one = np.array([-0.09720122, 0.05995294, 0.17654618, 0.21168263, 0.21638731, 0.04500235, 0.09229392])                 
-two = np.array([0.00247639, -0.00004086, -0.00240656, -0.00306555, -0.00321041, 0.00094253, 0.00012902])                   
-three = np.array([-0.00001842, -0.00000521, 0.00001039, 0.00001438, 0.00001579, -0.00001470, -0.00001169])             
-constant = np.array([3.41e+00, 0.69689692, -0.78761958, -1.11e+00, -0.93939272, 1.60e+00, 1.89e+00])
-ages = np.linspace(21, 80, 60)
-ages = np.tile(ages.reshape(60, 1), (1, 7))
-income_profiles = constant + one * ages + two * ages ** 2 + three * ages ** 3
-income_profiles = np.exp(income_profiles)
+earn_rate = pd.read_table(
+    "data/e_vec_data/cwhs_earn_rate_age_profile.csv", sep=',', header=0)
+del earn_rate['obs_earn']
+piv = earn_rate.pivot(index='age', columns='q_earn', values='mean_earn_rate')
 
+emat_basic = np.array(piv)
 
 '''
 ------------------------------------------------------------------------
@@ -56,29 +57,56 @@ and J, the ability matrix is created.
 '''
 
 
+def fit_exp_right(params, pt1):
+    a, b = params
+    x1, y1, slope = pt1
+    error1 = -a*b**(-x1)*np.log(b) - slope
+    error2 = a*b**(-x1) - y1
+    return [error1, error2]
+
+
+def exp_funct(points, a, b):
+    y = a*b**(-points)
+    return y
+
+
+def exp_fit(e_input, S, J):
+    params_guess = [20, 1]
+    e_output = np.zeros((S, J))
+    e_output[:50, :] = e_input
+    for j in xrange(J):
+        meanslope = np.mean([e_input[-1, j]-e_input[-2, j], e_input[
+            -2, j]-e_input[-3, j], e_input[-3, j]-e_input[-4, j]])
+        slope = np.min([meanslope, -.01])
+        a, b = opt.fsolve(fit_exp_right, params_guess, args=(
+            [70, e_input[-1, j], slope]))
+        e_output[50:, j] = exp_funct(np.linspace(70, 100, 30), a, b)
+    return e_output
+
+
 def graph_income(S, J, e, starting_age, ending_age, bin_weights):
     '''
     Graphs the log of the ability matrix.
     '''
+    e_tograph = np.log(e)
     domain = np.linspace(starting_age, ending_age, S)
     Jgrid = np.zeros(J)
     for j in xrange(J):
         Jgrid[j:] += bin_weights[j]
     X, Y = np.meshgrid(domain, Jgrid)
-    cmap2 = matplotlib.cm.get_cmap('autumn')
+    cmap2 = matplotlib.cm.get_cmap('summer')
     if J == 1:
         plt.figure()
-        plt.plot(domain, np.log(e))
+        plt.plot(domain, e_tograph)
         plt.savefig('OUTPUT/Demographics/ability_log')
     else:
         fig10 = plt.figure()
         ax10 = fig10.gca(projection='3d')
-        ax10.plot_surface(X, Y, np.log(e).T, rstride=1, cstride=2, cmap=cmap2)
+        ax10.plot_surface(X, Y, e_tograph.T, rstride=1, cstride=2, cmap=cmap2)
         ax10.set_xlabel(r'age-$s$')
         ax10.set_ylabel(r'ability type -$j$')
         ax10.set_zlabel(r'log ability $log(e_j(s))$')
-        plt.show()
-        # plt.savefig('OUTPUT/Demographics/ability_log')
+        plt.savefig('OUTPUT/Demographics/ability_log')
     if J == 1:
         plt.figure()
         plt.plot(domain, e)
@@ -90,38 +118,7 @@ def graph_income(S, J, e, starting_age, ending_age, bin_weights):
         ax10.set_xlabel(r'age-$s$')
         ax10.set_ylabel(r'ability type -$j$')
         ax10.set_zlabel(r'ability $e_j(s)$')
-        # plt.savefig('OUTPUT/Demographics/ability')
-        plt.show()
-
-
-def arc_tan_func(points, a, b, c):
-    y = (-a / np.pi) * np.arctan(b*points + c) + a / 2
-    return y
-
-
-def arc_tan_deriv_func(points, a, b, c):
-    y = -a * b / (np.pi * (1+(b*points+c)**2))
-    return y
-
-
-def arc_error(guesses, params):
-    a, b, c = guesses
-    first_point, coef1, coef2, coef3 = params
-    error1 = first_point - arc_tan_func(80, a, b, c)
-    error2 = np.exp(3 * coef3 * 80 ** 2 + 2 * coef2 * 80 + coef1) - arc_tan_deriv_func(80, a, b, c)
-    error3 = .1 * first_point - arc_tan_func(95, a, b, c)
-    error = [np.abs(error1)] + [np.abs(error2)] + [np.abs(error3)]
-    print np.array(error).max()
-    return error
-
-
-def arc_tan_fit(first_point, coef1, coef2, coef3):
-    guesses = [200, 2, 2]
-    params = [first_point, coef1, coef2, coef3]
-    a, b, c = opt.fsolve(arc_error, guesses, params)
-    print a, b, c
-    old_ages = np.linspace(81, 100, 20)
-    return arc_tan_func(old_ages, a, b, c)
+        plt.savefig('OUTPUT/Demographics/ability')
 
 
 def get_e(S, J, starting_age, ending_age, bin_weights, omega_SS):
@@ -137,13 +134,28 @@ def get_e(S, J, starting_age, ending_age, bin_weights, omega_SS):
                     age cohort, normalized so
                     the mean is one
     '''
-    e_short = income_profiles
-    e_final = np.ones((S, J))
-    e_final[:60, :] = e_short
-    e_final[60:, :] = 0.0
+    emat_trunc = emat_basic[:50, :]
+    cum_bins = 100 * np.array(bin_weights)
+    for j in xrange(J-1):
+        cum_bins[j+1] += cum_bins[j]
+    emat_collapsed = np.zeros((50, J))
+    for s in xrange(50):
+        for j in xrange(J):
+            if j == 0:
+                emat_collapsed[s, j] = emat_trunc[s, :cum_bins[j]].mean()
+            else:
+                emat_collapsed[s, j] = emat_trunc[
+                    s, cum_bins[j-1]:cum_bins[j]].mean()
+    e_fitted = np.zeros((50, J))
     for j in xrange(J):
-        e_final[60:, j] = arc_tan_fit(e_final[59, j], one[j], two[j], three[j])
-    graph_income(S, J, e_final, starting_age, ending_age, bin_weights)
-    return e_final
+        func = poly.polyfit(
+            np.arange(50)+starting_age, emat_collapsed[:50, j], deg=2)
 
-get_e(80, 7, 21, 100, np.array([.25, .25, .2, .1, .1, .09, .01]), 0)
+        e_fitted[:, j] = poly.polyval(np.arange(50)+starting_age, func)
+    emat_extended = exp_fit(e_fitted, S, J)
+    for j in xrange(1, J):
+        emat_extended[:, j] = np.max(np.array(
+            [emat_extended[:, j], emat_extended[:, j-1]]), axis=0)
+    graph_income(S, J, emat_extended, starting_age, ending_age, bin_weights)
+    emat_normed = emat_extended/(omega_SS * emat_extended).sum()
+    return emat_normed
