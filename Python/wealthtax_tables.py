606f82f04ef32b5550cd080a72ff2b7329dc0bbe
Jason DeBacker <jason.debacker@a02655jdebacker.attlocal.net>
2016-09-18 21:14:17 -0400

    Table 3 output to excel

--- Python/wealthtax_tables.py
+++ Python/wealthtax_tables.py
@@ -16,10 +16,10 @@ This py-file calls the following other file(s):
 '''
 import numpy as np
 import pandas as pd
-import utils
 import os
 from scipy import stats
 import cPickle as pickle
+import xlsxwriter
 
 from ogusa import parameters, labor, utils, demographics
 
@@ -47,25 +47,32 @@ l_tilde = run_params['ltilde']
 theta = 1/run_params['frisch']
 b_ellipse = run_params['b_ellipse']
 upsilon = run_params['upsilon']
-
-ss_dir = os.path.join(baseline_dir, "sigma3.0/SS/SS_vars.pkl")
-ss_output = pickle.load(open(ss_dir, "rb"))
-bssmat_base = ss_output['bssmat']
-factor_base = ss_output['factor_ss']
-n_base = ss_output['nssmat']
-c_base = ss_output['cssmat']
-BQ_base = ss_output['BQss']
-#utility_base = ss_output[]
-#income_base = ss_output[]
-Kss_base = ss_output['Kss']
-Lss_base = ss_output['Lss']
-
+omega_SS = run_params['omega_SS']
+e = run_params['e']
 
 bssmat = {}
 factor = {}
 n = {}
 c = {}
 BQ = {}
+Kss = {}
+Lss = {}
+wss = {}
+rss = {}
+ss_dir = os.path.join(baseline_dir, "sigma3.0/SS/SS_vars.pkl")
+ss_output = pickle.load(open(ss_dir, "rb"))
+bssmat['base'] = ss_output['bssmat']
+factor['base'] = ss_output['factor_ss']
+n['base'] = ss_output['nssmat']
+c['base'] = ss_output['cssmat']
+BQ['base'] = ss_output['BQss']
+#utility_base = ss_output[]
+#income_base = ss_output[]
+Kss['base'] = ss_output['Kss']
+Lss['base'] = ss_output['Lss']
+wss['base'] = ss_output['wss']
+rss['base'] = ss_output['rss']
+
 for item in ('wealth','income'):
     ss_dir = os.path.join(reform_dir[item], "sigma3.0/SS/SS_vars.pkl")
     ss_output = pickle.load(open(ss_dir, "rb"))
@@ -76,17 +83,69 @@ for item in ('wealth','income'):
     BQ[item] = ss_output['BQss']
     #utility[item] = ss_output[]
     #income[item] = ss_output[]
+    wss[item] = ss_output['wss']
+    rss[item] = ss_output['rss']
 
 
 '''
 ------------------------------------------------------------------------
-    Tables
+    Tables - all tables for paper saved to different worksheets in an
+    Excel workbook
 ------------------------------------------------------------------------
 '''
-## Moments, data vs model - plus minstat from GMM estiation (wealth moments only)
+## open Excel workbook
+workbook = xlsxwriter.Workbook('WealthTaxTables.xlsx')
 
-## Comparision on changes in the SS gini (total, by age, by type) from baseline
+## Moments, data vs model - plus minstat from GMM estimation (wealth moments only)
+
+
+## Comparision of changes in the SS gini (total, by age, by type) from baseline
 ## vs wealth tax and income tax reforms
+weights = {}
+b_dict = {}
+y_dict = {}
+c_dict = {}
+n_dict = {}
+gini = {}
+weights['total'] = np.tile(omega_SS.reshape(S, 1), (1, J)) * lambdas.reshape(1, J)
+weights['ability'] = lambdas
+weights['age'] = omega_SS
+for tax_run in ('base','wealth','income'):
+    income = ((wss[tax_run]*e*n[tax_run]) + (rss[tax_run]*bssmat[tax_run]))*factor['base']
+    b_dict[tax_run,'total'] = bssmat[tax_run]
+    b_dict[tax_run,'ability'] = bssmat[tax_run].sum(axis=0)
+    b_dict[tax_run,'age'] = bssmat[tax_run].sum(axis=1)
+    y_dict[tax_run,'total'] = income
+    y_dict[tax_run,'ability'] = income.sum(axis=0)
+    y_dict[tax_run,'age'] = income.sum(axis=1)
+    c_dict[tax_run,'total'] = c[tax_run]
+    c_dict[tax_run,'ability'] = c[tax_run].sum(axis=0)
+    c_dict[tax_run,'age'] = c[tax_run].sum(axis=1)
+    n_dict[tax_run,'total'] = n[tax_run]
+    n_dict[tax_run,'ability'] = n[tax_run].sum(axis=0)
+    n_dict[tax_run,'age'] = n[tax_run].sum(axis=1)
+    for item in ('total','ability','age'):
+        gini['b',item,tax_run] = utils.gini(b_dict[tax_run,item], weights[item])
+        gini['y',item,tax_run] = utils.gini(y_dict[tax_run,item], weights[item])
+        gini['c',item,tax_run] = utils.gini(b_dict[tax_run,item], weights[item])
+        gini['n',item,tax_run] = utils.gini(n_dict[tax_run,item], weights[item])
+
+# write to workbook
+worksheet = workbook.add_worksheet('Table 3')
+row = 2
+col = 2
+for var in ('b','y','c','n'):
+    row+=1
+    for item in ('total','ability','age'):
+        row+=1
+        col = 2
+        for tax_run in ('base','wealth','income'):
+            worksheet.write(row,col,gini[(var,item,tax_run)])
+            col += 2
+workbook.close()
+quit()
+
+
 
 ## Compare changes in aggregate variables in SS - baseline vs two reforms
 
